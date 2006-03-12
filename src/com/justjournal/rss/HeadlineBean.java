06c708840c6985f11c6aeede1859e0c27f363215
laffer1 <laffer1>
2006-03-12 04:27:29 +0000

    Moved RSS related code into its own package.

---
+++ src/com/justjournal/rss/HeadlineBean.java
@@ -0,0 +1,312 @@
+/*
+Copyright (c) 2003-2006, Lucas Holt
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+  Redistributions of source code must retain the above copyright notice, this list of
+  conditions and the following disclaimer.
+
+  Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or other
+  materials provided with the distribution.
+
+  Neither the name of the Just Journal nor the names of its contributors
+  may be used to endorse or promote products derived from this software without
+  specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package com.justjournal.rss;
+
+import com.justjournal.utility.Xml;
+import org.apache.log4j.Category;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Retrieves a RSS document using HTTP, parses the document, and
+ * converts it to HTML.
+ *
+ * @author Lucas Holt
+ * @version 1.4
+ * @since 1.0
+ *        User: laffer1
+ *        Date: Jul 22, 2003
+ *        Time: 12:19:17
+ *        <p/>
+ *        1.4 Fixed bugs when certain rss features are missing.
+ *        1.3 now supports several RSS 2 features (non rdf format)
+ *        1.2 added several properties to the output including
+ *        the published date, and description.
+ *        1.1 optimized code
+ *        1.0 Initial release
+ */
+
+public class HeadlineBean {
+
+    private static Category log = Category.getInstance(HeadlineBean.class.getName());
+
+    private static final char endl = '\n';
+
+    protected URL u;
+    protected InputStream inputXML;
+    protected DocumentBuilder builder;
+    protected Document document;
+
+    final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+    protected void getRssDocument(final String uri)
+            throws Exception {
+
+        if (log.isDebugEnabled())
+            log.debug("Starting getRssDocument()");
+
+        //Open the file for reading:
+        u = new URL(uri);
+        inputXML = u.openStream();
+
+        //Build document:
+        factory.setValidating(false);
+        builder = factory.newDocumentBuilder();
+        document = builder.parse(inputXML);
+    }
+
+    public String parse(final String url) {
+        try {
+            if (log.isDebugEnabled())
+                log.debug("Starting parse()");
+
+            getRssDocument(url);
+
+            // output variable
+            final StringBuffer sb = new StringBuffer();
+
+            String contentTitle = "";
+            String contentLink = "";
+            // String contentDescription = "";
+            String contentLastBuildDate = "";
+            String contentGenerator = "";
+
+            if (log.isDebugEnabled())
+                log.debug("Prepare xml nodelists");
+
+            org.w3c.dom.NodeList channelList = document.getElementsByTagName("channel");
+            org.w3c.dom.NodeList chnodes = channelList.item(0).getChildNodes();
+
+            String imageUrl = null;
+            String imageTitle = null;
+            String imageLink = "";
+            String imageWidth = null;
+            String imageHeight = null;
+
+            if (log.isDebugEnabled())
+                log.debug("Iterate through the nodelists");
+
+            for (int k = 0; k < chnodes.getLength(); k++) {
+                org.w3c.dom.Node curNode = chnodes.item(k);
+
+                if (curNode.getNodeName().equals("title")) {
+                    contentTitle = curNode.getChildNodes().item(0).getNodeValue();
+                } else if (curNode.getNodeName().equals("link")) {
+                    contentLink = curNode.getChildNodes().item(0).getNodeValue();
+                    //} else if (curNode.getNodeName().equals("description")) {
+                    //    contentDescription = curNode.getChildNodes().item(0).getNodeValue();
+                } else if (curNode.getNodeName().equals("lastBuildDate")) {
+                    contentLastBuildDate = curNode.getChildNodes().item(0).getNodeValue();
+                } else if (curNode.getNodeName().equals("generator")) {
+                    contentGenerator = curNode.getChildNodes().item(0).getNodeValue();
+                } else if (curNode.getNodeName().equals("image")) {
+
+                    org.w3c.dom.NodeList imageNodes = curNode.getChildNodes();
+
+                    for (int z = 0; z < imageNodes.getLength(); z++) {
+                        if (imageNodes.item(z).getNodeName().equals("url")) {
+                            imageUrl = imageNodes.item(z).getChildNodes().item(0).getNodeValue();
+                        } else if (imageNodes.item(z).getNodeName().equals("height")) {
+                            imageHeight = imageNodes.item(z).getChildNodes().item(0).getNodeValue();
+                        } else if (imageNodes.item(z).getNodeName().equals("width")) {
+                            imageWidth = imageNodes.item(z).getChildNodes().item(0).getNodeValue();
+                        } else if (imageNodes.item(z).getNodeName().equals("title")) {
+                            imageTitle = imageNodes.item(z).getChildNodes().item(0).getNodeValue();
+                        } else if (imageNodes.item(z).getNodeName().equals("link")) {
+                            imageLink = imageNodes.item(z).getChildNodes().item(0).getNodeValue();
+                        }
+                    }
+                }
+            }
+
+            if (log.isDebugEnabled())
+                log.debug("Prepare HTML output");
+
+            // create header!
+            sb.append("<div style=\"width: 100%; padding: .1in; background: #F2F2F2;\" class=\"ljfhead\">");
+            sb.append(endl);
+
+            sb.append("<!-- Generator: ");
+            sb.append(contentGenerator);
+            sb.append(" -->");
+            sb.append(endl);
+
+            if (imageUrl != null) {
+                sb.append("<span style=\"padding: 5px; float:left; width:");
+                sb.append(imageWidth);
+                sb.append("px; height:");
+                sb.append(imageHeight);
+                sb.append("px; position: relative;\">");
+
+                sb.append("<a href=\"");
+                sb.append(imageLink);
+                sb.append("\">");
+
+                sb.append("<img src=\"");
+                sb.append(imageUrl);
+                sb.append("\" height=\"");
+                sb.append(imageHeight);
+                sb.append("\" width=\"");
+                sb.append(imageWidth);
+                sb.append("\" alt=\"");
+                sb.append(imageTitle);
+                sb.append("\" /></a></span>");
+                sb.append(endl);
+            }
+
+            sb.append("<h3>");
+            sb.append(contentTitle);
+            sb.append("</h3>");
+            sb.append(endl);
+
+            // some rss feeds don't have a last build date
+            if (contentLastBuildDate != null) {
+                sb.append("<p>last build date: ");
+                sb.append(contentLastBuildDate);
+                sb.append("<br />");
+            } else {
+                sb.append("<p>");
+            }
+
+            /* if (contentDescription != null) {
+                sb.append(contentDescription);
+                sb.append("<br />");
+            }*/
+
+            if (contentLink != null) {
+                sb.append("<a href=\"").append(contentLink).append("\">source</a>");
+            }
+            sb.append("</p>");
+            sb.append(endl);
+
+
+            sb.append("<div style=\"clear: both;\">&nbsp;</div>");
+            sb.append(endl);
+
+            sb.append("</div>");
+            sb.append(endl);
+
+            //Generate the NodeList;
+            org.w3c.dom.NodeList nodeList = document.getElementsByTagName("item");
+
+            sb.append("<ul class=\"RssItems\">");
+            sb.append(endl);
+
+            for (int i = 0; i < nodeList.getLength() && i < 16; i++) {
+                org.w3c.dom.NodeList childList = nodeList.item(i).getChildNodes();
+                // some of the properties of <items>
+                String link = null;
+                String title = null;
+                String pubDate = null;
+                String description = null;
+                String guid = null;
+
+                for (int j = 0; j < childList.getLength(); j++) {
+                    if (childList.item(j).getNodeName().equals("link")) {
+                        link = childList.item(j).getChildNodes().item(0).getNodeValue();
+                    } else if (childList.item(j).getNodeName().equals("title")) {
+                        title = childList.item(j).getChildNodes().item(0).getNodeValue();
+
+                    } else if (childList.item(j).getNodeName().equals("pubDate")) {
+                        pubDate = childList.item(j).getChildNodes().item(0).getNodeValue();
+
+                    } else if (childList.item(j).getNodeName().equals("description")) {
+                        description = childList.item(j).getChildNodes().item(0).getNodeValue();
+
+                    } else if (childList.item(j).getNodeName().equals("guid")) {
+                        guid = childList.item(j).getChildNodes().item(0).getNodeValue();
+                    }
+                }
+
+                // assert the basic properties are there.
+                if (link != null && title != null) {
+                    sb.append("<li>");
+
+                    sb.append("<span class=\"RssItemTitle\">");
+                    sb.append("<a href=\"");
+                    sb.append(link);
+                    sb.append("\" title=\"");
+                    sb.append(Xml.cleanString(title));
+                    sb.append("\" >");
+                    sb.append(Xml.cleanString(title));
+                    sb.append("</a>");
+                    sb.append("</span> ");
+
+                    // some rss versions don't have a pub date per entry
+                    if (pubDate != null) {
+                        sb.append(" <span class=\"RssItemPubDate\">");
+                        sb.append(pubDate);
+                        sb.append("</span> ");
+                    }
+
+                    sb.append("<br />");
+                    sb.append("<span class=\"RssItemDesc\">");
+                    /*
+                      /<(script|noscript|object|embed|style|frameset|frame|iframe)[>\s\S]*<\/\\1>/i /<\/?!?(param|link|meta|doctype|div|font)[^>]*>/i /(class|style|id)=\"[^\"]*\"/i
+                    */
+                    Pattern p = Pattern.compile("/<(script|noscript|object|embed|style|frameset|frame|iframe|link)[>\\s\\S]*<\\/\\1>/i");
+                    Matcher m = p.matcher(description);
+                    String result = m.replaceAll("");
+
+                    sb.append(result);
+                    sb.append("</span>");
+
+                    sb.append("<span class=\"RssReadMore\"><a href=\"");
+                    sb.append("<a href=\"");
+                    sb.append(guid);
+                    sb.append("\">");
+                    sb.append("Read More</a></span>");
+
+                    sb.append("</li>");
+                    sb.append(endl);
+                }
+            }
+
+            sb.append("</ul>");
+            sb.append(endl);
+
+            return sb.toString();
+        } catch (Exception e) {
+            return "Error, could not process request: " + e.toString() + endl;
+        }
+    }
+
+}
---
+++ src/com/justjournal/rss/Rss.java
@@ -0,0 +1,286 @@
+/*
+Copyright (c) 2003-2006, Lucas Holt
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+  Redistributions of source code must retain the above copyright notice, this list of
+  conditions and the following disclaimer.
+
+  Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or other
+  materials provided with the distribution.
+
+  Neither the name of the Just Journal nor the names of its contributors
+  may be used to endorse or promote products derived from this software without
+  specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package com.justjournal.rss;
+
+import com.justjournal.db.EntryTo;
+import com.justjournal.utility.Xml;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * @author Lucas Holt
+ * @version 1.1
+ * @since 1.0
+ *        User: laffer1
+ *        Date: Aug 27, 2003
+ *        Time: 11:54:38 PM
+ */
+public final class Rss {
+    private final static int MAX_LENGTH = 15; // max size of RSS content
+
+    private String title;
+    private String link;
+    private String description;
+    private String language;
+    private String copyright;
+    private String webMaster;
+    private String managingEditor;
+
+    private ArrayList items = new ArrayList(MAX_LENGTH);
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getManagingEditor() {
+        return managingEditor;
+    }
+
+    public void setManagingEditor(String managingEditor) {
+        this.managingEditor = managingEditor;
+    }
+
+    public String getWebMaster() {
+        return webMaster;
+    }
+
+    public void setWebMaster(String webMaster) {
+        this.webMaster = webMaster;
+    }
+
+    // Methods
+
+    public void populate(Collection entries) {
+
+        RssItem item;
+
+        // TODO: this sucks... need to make this reusable
+        try {
+
+            EntryTo o;
+            Iterator itr = entries.iterator();
+
+            for (int x = 0, n = entries.size(); x < n && x < MAX_LENGTH; x++) {
+                o = (EntryTo) itr.next();
+                item = new RssItem();
+                item.setTitle(o.getSubject());
+                item.setLink("http://www.justjournal.com/Users/" + o.getUserName());
+                item.setDescription(o.getBody());
+                item.setGuid("http://www.justjournal.com/Users/" + o.getUserName() + "/entry/" + o.getId());
+                item.setPubDate(o.getDate().toPubDate());
+                Add(item);
+            }
+
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void Add(RssItem item) {
+        items.add(item);
+    }
+
+    public String toXml() {
+
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n");
+
+        sb.append("<rss version=\"2.0\">\n");
+        sb.append("\t<channel>\n");
+
+        sb.append("\t\t<title>");
+        sb.append(Xml.cleanString(title));
+        sb.append("</title>\n");
+
+        sb.append("\t\t<link>");
+        sb.append(link);
+        sb.append("</link>\n");
+
+        sb.append("\t\t<description>");
+        sb.append(Xml.cleanString(description));
+        sb.append("</description>\n");
+
+        sb.append("\t\t<language>");
+        sb.append(language);
+        sb.append("</language>\n");
+
+        sb.append("\t\t<webMaster>");
+        sb.append(webMaster);
+        sb.append("</webMaster>\n");
+
+        sb.append("\t\t<managingEditor>");
+        sb.append(managingEditor);
+        sb.append("</managingEditor>\n");
+
+        sb.append("\t\t<copyright>");
+        sb.append(copyright);
+        sb.append("</copyright>\n");
+
+        sb.append("\t\t<generator>JustJournal v1.0</generator>\n");
+        sb.append("\t\t<docs>http://blogs.law.harvard.edu/tech/rss</docs>\n");
+        sb.append("\t\t<ttl>360</ttl>\n\n");
+
+        sb.append("\t\t<image>\n");
+
+        sb.append("\t\t\t<url>");
+        sb.append("http://www.justjournal.com/images/userclass_32.png");
+        sb.append("</url>\n");
+
+        sb.append("\t\t\t<title>");
+        sb.append(Xml.cleanString(title));
+        sb.append("</title>\n");
+
+        sb.append("\t\t\t<link>");
+        sb.append(link);
+        sb.append("</link>\n");
+
+        // max width 144, default if not here is 88
+        sb.append("\t\t\t<width>");
+        sb.append(32);
+        sb.append("</width>\n");
+
+        // max height is 400, default is 31
+        sb.append("\t\t\t<height>");
+        sb.append(32);
+        sb.append("</height>\n");
+
+        sb.append("\t\t</image>\n");
+
+        // last build date
+        // Sat, 07 Sep 2002 09:43:33 GMT
+        // someday get this format right
+        sb.append("\t\t<lastBuildDate>");
+        sb.append(date());
+        sb.append("</lastBuildDate>\n");
+
+        /* Iterator */
+        RssItem o;
+        Iterator itr = items.listIterator();
+        for (int i = 0, n = items.size(); i < n && i < MAX_LENGTH; i++)     // 15 is the limit for RSS
+        {
+            o = (RssItem) itr.next();
+
+            sb.append("\t\t<item>\n");
+
+            sb.append("\t\t\t<title>");
+            sb.append(Xml.cleanString(o.getTitle()));
+            sb.append("</title>\n");
+
+            sb.append("\t\t\t<link>");
+            sb.append(o.getLink());
+            sb.append("</link>\n");
+
+            sb.append("\t\t\t<description>");
+            sb.append(Xml.cleanString(o.getDescription()));
+            sb.append("...</description>\n");
+
+            sb.append("\t\t\t<guid isPermaLink=\"true\">");
+            sb.append(o.getGuid());
+            sb.append("</guid>\n");
+
+            sb.append("\t\t\t<pubDate>");
+            sb.append(o.getPubDate());
+            sb.append("</pubDate>\n");
+
+            sb.append("\t\t</item>\n");
+        }
+
+        sb.append("\t</channel>\n");
+        sb.append("</rss>\n");
+
+        return sb.toString();
+    }
+
+    private String date() {
+        //Sat, 07 Sep 2002 09:43:33 GMT
+        Calendar cal = new GregorianCalendar(java.util.TimeZone.getDefault());
+        java.util.Date current = cal.getTime();
+        final SimpleDateFormat formatmydate = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zz");
+
+        return formatmydate.format(current);
+    }
+
+    public void recycle() {
+        title = "";
+        link = "";
+        description = "";
+        language = "en-us";
+        copyright = "";
+        webMaster = "";
+        managingEditor = "";
+
+        items.clear();
+    }
+
+}
