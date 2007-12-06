6393af54129aa947d6fd502315a34655d04858ec
laffer1 <laffer1>
2007-12-06 02:00:16 +0000

    no message

---
+++ src/com/justjournal/ctl/XmlRpc.java
@@ -0,0 +1,60 @@
+/*
+Copyright (c) 2007, Lucas Holt
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+  Redistributions of source code must retain the above copyright notice, this list of
+  conditions and the following disclaimer.
+
+  Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or other
+  materials provided with the distribution.
+
+  Neither the name of the Just Journal nor the names of its contributors
+  may be used to endorse or promote products derived from this software without
+  specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package com.justjournal.ctl;
+
+import java.util.Random;
+import java.util.HashMap;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import redstone.xmlrpc.XmlRpcServlet;
+import com.justjournal.google.Blogger;
+
+/**
+ * User: laffer1
+ * Date: Dec 3, 2007
+ * Time: 4:13:53 PM
+ * $Id: XmlRpc.java,v 1.1 2007/12/06 02:00:15 laffer1 Exp $
+ *
+ */
+public class XmlRpc extends XmlRpcServlet {
+    public void init( ServletConfig servletConfig ) throws ServletException
+    {
+        super.init( servletConfig );
+
+        getXmlRpcServer().addInvocationHandler("blogger", new Blogger());
+        getXmlRpcServer().addInvocationHandler( "SimpleDatabase", new HashMap() );
+        getXmlRpcServer().addInvocationHandler( "RandomNumberGenerator", new Random() );
+    }
+}
---
+++ src/com/justjournal/google/Blogger.java
@@ -0,0 +1,233 @@
+package com.justjournal.google;
+
+
+import com.justjournal.db.UserTo;
+import com.justjournal.db.EntryTo;
+import com.justjournal.db.EntryDAO;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.justjournal.db.UserDao.*;
+import com.justjournal.User;
+import com.justjournal.WebError;
+import com.justjournal.WebLogin;
+import com.justjournal.utility.StringUtil;
+
+/**
+ * User: laffer1
+ * Date: Dec 3, 2007
+ * Time: 4:21:42 PM
+ * $Id: Blogger.java,v 1.1 2007/12/06 02:00:16 laffer1 Exp $
+ *
+ * A blogger 1 compatible interface
+ *
+ */
+public class Blogger {
+
+    public String getUsersInfo( String appkey, String username, String password)
+    {
+        String result = "";
+        int userId;
+        boolean blnError = false;
+
+
+        if (!StringUtil.lengthCheck(username, 3, 15)) {
+            blnError = true;
+        }
+
+        if (!StringUtil.lengthCheck(password, 5, 18)) {
+            blnError = true;
+        }
+
+        userId = WebLogin.validate(username, password);
+
+        try {
+            User user = new User(userId);
+
+            result = "<struct>\n";
+
+            result += "<member>\n";
+            result += "<name>" + "nickname" + "</name>\n";
+            result += "<value>" + user.getUserName() + "</value>\n";
+            result += "</member>\n";
+
+            result += "<member>\n";
+            result += "<name>" + "userid" + "</name>\n";
+            result += "<value>" + userId + "</value>\n";
+            result += "</member>\n";
+
+            result += "<member>\n";
+            result += "<name>" + "url" + "</name>\n";
+            result += "<value>" + "http://www.justjournal.com/users/" + user.getUserName() + "</value>\n";
+            result += "</member>\n";
+
+            result += "<member>\n";
+            result += "<name>" + "email" + "</name>\n";
+            result += "<value>" + user.getEmailAddress() + "</value>\n";
+            result += "</member>\n";
+
+            result += "<member>\n";
+            result += "<name>" + "firstname" + "</name>\n";
+            result += "<value>" + user.getFirstName() + "</value>\n";
+            result += "</member>\n";
+
+            result += "</struct>\n";
+
+        } catch (Exception e) {
+            blnError = true;
+        }
+
+        if (blnError)
+        {
+            result = "<struct>\n";
+
+            result += "<member>\n";
+            result += "<name>" + "faultCode" + "</name>\n";
+            result += "<value><int>4</int>" + "</value>\n";
+            result += "</member>\n";
+
+            result += "<member>\n";
+            result += "<name>" + "faultString" + "</name>\n";
+            result += "<value><string>" + "User authentication failed: " + username + "</string></value>\n";
+            result += "</member>\n";
+
+            result += "</struct>\n";
+
+        }
+
+
+        return result;
+    }
+
+    public ArrayList getUsersBlogs( String appkey, String username, String password)
+    {
+        String result = "";
+        int userId;
+        boolean blnError = false;
+        ArrayList a = new ArrayList();
+
+
+        if (!StringUtil.lengthCheck(username, 3, 15)) {
+            blnError = true;
+        }
+
+        if (!StringUtil.lengthCheck(password, 5, 18)) {
+            blnError = true;
+        }
+
+        userId = WebLogin.validate(username, password);
+
+        try {
+            User user = new User(userId);
+
+            HashMap s = new HashMap();
+            //s.put("url",)
+
+            result = "<array><data><value><struct>\n";
+
+            result += "<member>\n";
+            result += "<name>" + "url" + "</name>\n";
+            result += "<value>" + "http://www.justjournal.com/users/" + user.getUserName() + "</value>\n";
+            result += "</member>\n";
+
+            result += "<member>\n";
+            result += "<name>" + "blogid" + "</name>\n";
+            result += "<value>" + userId + "</value>\n";
+            result += "</member>\n";
+
+
+            result += "<member>\n";
+            result += "<name>" + "blogName" + "</name>\n";
+            result += "<value>" + user.getJournalName() + "</value>\n";
+            result += "</member>\n";
+
+            result += "</struct></value></data></array>\n";
+
+        } catch (Exception e) {
+            blnError = true;
+        }
+
+        if (blnError)
+        {
+            result = "<struct>\n";
+
+            result += "<member>\n";
+            result += "<name>" + "faultCode" + "</name>\n";
+            result += "<value><int>4</int>" + "</value>\n";
+            result += "</member>\n";
+
+            result += "<member>\n";
+            result += "<name>" + "faultString" + "</name>\n";
+            result += "<value><string>" + "User authentication failed: " + username + "</string></value>\n";
+            result += "</member>\n";
+
+            result += "</struct>\n";
+
+        }
+
+
+        return a;
+    }
+
+
+
+    public String newPost(String appkey, String blogid, String username, String password, String content, Boolean publish)
+    {
+        String result = "";
+        int userId;
+        boolean blnError = false;
+        final EntryTo et = new EntryTo();
+
+
+        if (!StringUtil.lengthCheck(username, 3, 15)) {
+            blnError = true;
+        }
+
+        if (!StringUtil.lengthCheck(password, 5, 18)) {
+            blnError = true;
+        }
+
+        userId = WebLogin.validate(username, password);
+
+        try {
+            User user = new User(userId);
+            et.setUserId(userId);
+               // et.setDate(java.util.Date().Now());
+             //   et.setSubject(StringUtil.replace(request.getParameter("subject"), '\'', "\\\'"));
+                et.setBody(StringUtil.replace(content, '\'', "\\\'"));
+                et.setAutoFormat(true);
+                et.setAllowComments(true);
+                et.setEmailComments(true);
+
+            EntryDAO.add(et);
+            result = "12345";
+
+        } catch (Exception e) {
+            blnError = true;
+        }
+
+        if (blnError)
+        {
+            result = "<struct>\n";
+
+            result += "<member>\n";
+            result += "<name>" + "faultCode" + "</name>\n";
+            result += "<value><int>4</int>" + "</value>\n";
+            result += "</member>\n";
+
+            result += "<member>\n";
+            result += "<name>" + "faultString" + "</name>\n";
+            result += "<value><string>" + "User authentication failed: " + username + "</string></value>\n";
+            result += "</member>\n";
+
+            result += "</struct>\n";
+
+        }
+
+
+        return result;
+    }
+}
