e6caaeb7421a47946fb9c7e46bcf27afee54fbda
laffer1 <laffer1>
2007-12-06 19:43:36 +0000

    Add license. But initial editPost function (copy of new post for now) as a place holder.  We need to tweak this later. Tweak and add more comments.

--- src/com/justjournal/google/Blogger.java
+++ src/com/justjournal/google/Blogger.java
@@ -1,5 +1,38 @@
-package com.justjournal.google;
+/*
+Copyright (c) 2007, Lucas Holt
+All rights reserved.
 
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+  Redistributions of source code must retain the above copyright notice, this list of
+  conditions and the following disclaimer.
+
+  Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or other
+  materials provided with the distribution.
+
+  Neither the name of the Just Journal nor the names of its contributors
+  may be used to endorse or promote products derived from this software without
+  specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package com.justjournal.google;
 
 import com.justjournal.User;
 import com.justjournal.WebLogin;
@@ -17,9 +50,19 @@ import java.util.HashMap;
  * User: laffer1
  * Date: Dec 3, 2007
  * Time: 4:21:42 PM
- * $Id: Blogger.java,v 1.5 2007/12/06 19:32:26 laffer1 Exp $
+ * $Id: Blogger.java,v 1.6 2007/12/06 19:43:36 laffer1 Exp $
  * <p/>
  * A blogger 1 compatible interface exposed by XML-RPC
+ * <p/>
+ * http://www.blogger.com/developers/api/1_docs/
+ * <p/>
+ * Methods defined in Blogger 1.0 API:
+ * blogger.newPost: Makes a new post to a designated blog. Optionally, will publish the blog after making the post.
+ * blogger.editPost: Edits a given post. Optionally, will publish the blog after making the edit.
+ * blogger.getUsersBlogs: Returns information on all the blogs a given user is a member of.
+ * blogger.getUserInfo: Authenticates a user and returns basic user info (name, email, userid, etc.)
+ * blogger.getTemplate: Returns the main or archive index template of a given blog.
+ * blogger.setTemplate: Edits the main or archive index template of a given blog.
  */
 public class Blogger {
 
@@ -28,11 +71,11 @@ public class Blogger {
     /**
      * Fetch the users personal information including their username, userid,
      * email address and name.
-     *
+     * <p/>
      * The Blogger API defines nickname, userid, url, email, firstname and lastname here.  We don't track
      * last name.
      *
-     * @param appkey  Not used but required by Blogger API
+     * @param appkey   Not used but required by Blogger API
      * @param username username to authenticate/check
      * @param password the account secret
      * @return A HashMap of the users personal info or an error code as a hashmap
@@ -77,13 +120,13 @@ public class Blogger {
     }
 
 
-     /**
+    /**
      * Fetch the users blogs.  JJ only supports 1 blog per user right now.  Still this must be a list
      * of all blogs.
-     *
+     * <p/>
      * The Blogger API defines url, blogid and blogName.
      *
-     * @param appkey  Not used but required by Blogger API
+     * @param appkey   Not used but required by Blogger API
      * @param username username to authenticate/check
      * @param password the account secret
      * @return A list of hash maps for each blog
@@ -131,15 +174,92 @@ public class Blogger {
     /**
      * Create a new blog entry using the limited feautres of the blogger api.
      *
-     * @param appkey  Ignored but required
-     * @param blogid Ignored since JJ supports 1 blog per user.  Just the UID anyway.
+     * @param appkey   Ignored but required
+     * @param blogid   Ignored since JJ supports 1 blog per user.  Just the UID anyway.
      * @param username Username of blog owner.
      * @param password password of blog owner.
      * @param content  The body/content of the blog (no subjects)
      * @param publish  A boolean representing the publish state of the entry.  JJ does not support drafts yet.
-     * @return The entry id of the entry as a string or an error.  
+     * @return The entry id of the entry as a string or an error.
      */
     public String newPost(String appkey, String blogid, String username, String password, String content, Boolean publish) {
+        String result = "";
+        int userId;
+        boolean blnError = false;
+        final EntryTo et = new EntryTo();
+        EntryTo et2;
+
+        if (!StringUtil.lengthCheck(username, 3, 15)) {
+            blnError = true;
+        }
+
+        if (!StringUtil.lengthCheck(password, 5, 18)) {
+            blnError = true;
+        }
+
+        userId = WebLogin.validate(username, password);
+
+        try {
+            User user = new User(userId);
+            et.setUserId(userId);
+            DateTime d = new DateTimeBean();
+            d.set(new java.util.Date());
+            et.setDate(d);
+            //   et.setSubject(StringUtil.replace(request.getParameter("subject"), '\'', "\\\'"));
+            et.setBody(StringUtil.replace(content, '\'', "\\\'"));
+            //et.setMusic(StringUtil.replace(music, '\'', "\\\'"));
+            et.setSecurityLevel(2);   // public
+            et.setLocationId(0); // not specified
+            et.setMoodId(12);    // not specified
+            et.setAutoFormat(true);
+            et.setAllowComments(true);
+            et.setEmailComments(true);
+            et.setUserId(userId);
+            et.setUserName(user.getUserName());
+
+            EntryDAO.add(et);
+            et2 = EntryDAO.viewSingle(et);
+            result = Integer.toString(et2.getId());
+
+        } catch (Exception e) {
+            blnError = true;
+            log.debug(e.getMessage());
+        }
+
+        if (blnError) {
+            result = "<struct>\n";
+
+            result += "<member>\n";
+            result += "<name>" + "faultCode" + "</name>\n";
+            result += "<value><int>4</int>" + "</value>\n";
+            result += "</member>\n";
+
+            result += "<member>\n";
+            result += "<name>" + "faultString" + "</name>\n";
+            result += "<value><string>" + "User authentication failed: " + username + "</string></value>\n";
+            result += "</member>\n";
+
+            result += "</struct>\n";
+
+        }
+
+        return result;
+    }
+
+    /**
+     * Modify a blog entry using the limited feautres of the blogger api.
+     * <p/>
+     * TODO: make this work
+     *
+     * @param appkey   Ignored but required
+     * @param postid   entry id as a string
+     * @param username Username of blog owner.
+     * @param password password of blog owner.
+     * @param content  The body/content of the blog (no subjects)
+     * @param publish  A boolean representing the publish state of the entry.  JJ does not support drafts yet.
+     * @return The entry id of the entry as a string or an error.
+     */
+    public String editPost(String appkey, String postid, String username, String password, String content, Boolean publish) {
         String result = "";
         int userId;
         boolean blnError = false;
