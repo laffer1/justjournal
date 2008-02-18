3964ac35ef0ff20c1a4c4c1e6b10398cc4466bed
laffer1 <laffer1>
2008-02-18 03:56:09 +0000

    time zones and fixes

--- src/com/justjournal/utility/DateConvert.java
+++ src/com/justjournal/utility/DateConvert.java
@@ -11,17 +11,24 @@ import java.util.TimeZone;
  * Time: 11:52:55 PM
  *
  * @author Lucas Holt
- * @version $Id: DateConvert.java,v 1.2 2008/01/05 16:54:12 laffer1 Exp $
+ * @version $Id: DateConvert.java,v 1.3 2008/02/18 03:56:09 laffer1 Exp $
  */
 public final class DateConvert {
     private static final SimpleDateFormat df822 = new SimpleDateFormat("EEE', 'dd' 'MMM' 'yyyy' 'HH:mm:ss' 'Z", Locale.US);
     private static final SimpleDateFormat df3339 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
     public static String encode822() {
+        df822.setTimeZone(TimeZone.getTimeZone("GMT"));
         return df822.format(new Date()).toString();
     }
 
     public static String encode822(Date data) {
+        df822.setTimeZone(TimeZone.getTimeZone("GMT"));
+        return df822.format(data).toString();
+    }
+
+    public static String encode822(Date data, TimeZone tz) {
+        df822.setTimeZone(tz);
         return df822.format(data).toString();
     }
 
