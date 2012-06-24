c0e521260fb6a2b93ee0a1cc7f40b057e1dc897d
laffer1 <laffer1>
2012-06-24 16:45:58 +0000

    Get rid of unused setter methods.

--- src/com/justjournal/core/Settings.java
+++ src/com/justjournal/core/Settings.java
@@ -42,7 +42,7 @@ import java.util.List;
  * even if Users have them turned on.
  *
  * @author Lucas Holt
- * @version $Id: Settings.java,v 1.10 2012/06/23 18:15:31 laffer1 Exp $
+ * @version $Id: Settings.java,v 1.11 2012/06/24 16:45:58 laffer1 Exp $
  * @since 1.0
  */
 public class Settings {
@@ -164,6 +164,7 @@ public class Settings {
         }
     }
 
+    @SuppressWarnings("unchecked")
     public List<com.justjournal.model.Settings> list() throws CayenneRuntimeException {
         ObjectContext dataContext;
         try {
@@ -183,177 +184,106 @@ public class Settings {
         return baseUri;
     }
 
-    public void setBaseUri(String baseUri) {
-        this.baseUri = baseUri;
-    }
 
     public String getFsPath() {
         return fsPath;
-    }
-
-    public void setFsPath(String fsPath) {
-        this.fsPath = fsPath;
     }
 
     public String getContextPath() {
         return contextPath;
     }
 
-    public void setContextPath(String contextPath) {
-        this.contextPath = contextPath;
-    }
 
     public boolean isSiteEnable() {
         return siteEnable;
     }
 
-    public void setSiteEnable(boolean siteEnable) {
-        this.siteEnable = siteEnable;
-    }
 
     public String getSiteName() {
         return siteName;
     }
 
-    public void setSiteName(String siteName) {
-        this.siteName = siteName;
-    }
 
     public String getSiteAdmin() {
         return siteAdmin;
     }
 
-    public void setSiteAdmin(String siteAdmin) {
-        this.siteAdmin = siteAdmin;
-    }
 
     public String getSiteAdminEmail() {
         return siteAdminEmail;
     }
 
-    public void setSiteAdminEmail(String siteAdminEmail) {
-        this.siteAdminEmail = siteAdminEmail;
-    }
 
     public String getSiteBlog() {
         return siteBlog;
     }
 
-    public void setSiteBlog(String siteBlog) {
-        this.siteBlog = siteBlog;
-    }
 
     public boolean isSiteSearch() {
         return siteSearch;
     }
 
-    public void setSiteSearch(boolean siteSearch) {
-        this.siteSearch = siteSearch;
-    }
 
     public boolean isSiteDirectory() {
         return siteDirectory;
     }
 
-    public void setSiteDirectory(boolean siteDirectory) {
-        this.siteDirectory = siteDirectory;
-    }
 
     public boolean isSiteRss() {
         return siteRss;
     }
 
-    public void setSiteRss(boolean siteRss) {
-        this.siteRss = siteRss;
-    }
 
     public boolean isMailEnable() {
         return mailEnable;
-    }
-
-    public void setMailEnable(boolean mailEnable) {
-        this.mailEnable = mailEnable;
     }
 
     public String getMailHost() {
         return mailHost;
     }
 
-    public void setMailHost(String mailHost) {
-        this.mailHost = mailHost;
-    }
-
     public int getMailPort() {
         return mailPort;
-    }
-
-    public void setMailPort(int mailPort) {
-        this.mailPort = mailPort;
     }
 
     public String getMailUser() {
         return mailUser;
     }
 
-    public void setMailUser(String mailUser) {
-        this.mailUser = mailUser;
-    }
-
     public String getMailPass() {
         return mailPass;
     }
 
-    public void setMailPass(String mailPass) {
-        this.mailPass = mailPass;
-    }
 
     public String getMailFrom() {
         return mailFrom;
     }
 
-    public void setMailFrom(String mailFrom) {
-        this.mailFrom = mailFrom;
-    }
 
     public String getMailSubject() {
         return mailSubject;
     }
 
-    public void setMailSubject(String mailSubject) {
-        this.mailSubject = mailSubject;
-    }
 
     public boolean isCommentEnable() {
         return commentEnable;
     }
 
-    public void setCommentEnable(boolean commentEnable) {
-        this.commentEnable = commentEnable;
-    }
 
     public boolean isCommentMailEnable() {
         return commentMailEnable;
     }
 
-    public void setCommentMailEnable(boolean commentMailEnable) {
-        this.commentMailEnable = commentMailEnable;
-    }
 
     public boolean isCommentEnableAnonymous() {
         return commentEnableAnonymous;
     }
 
-    public void setCommentEnableAnonymous(boolean commentEnableAnonymous) {
-        this.commentEnableAnonymous = commentEnableAnonymous;
-    }
 
     public int getTzOffset() {
         return tzOffset;
     }
 
-    public void setTzOffset(int tzOffset) {
-        this.tzOffset = tzOffset;
-    }
 
     public String getTzName() {
         return tzName;
@@ -367,25 +297,15 @@ public class Settings {
         return tzLocalize;
     }
 
-    public void setTzLocalize(boolean tzLocalize) {
-        this.tzLocalize = tzLocalize;
-    }
-
     public boolean isTzUseGMT() {
         return tzUseGMT;
     }
 
-    public void setTzUseGMT(boolean tzUseGMT) {
-        this.tzUseGMT = tzUseGMT;
-    }
 
     public boolean isUserAllowNew() {
         return userAllowNew;
     }
 
-    public void setUserAllowNew(boolean userAllowNew) {
-        this.userAllowNew = userAllowNew;
-    }
 
     public static Settings getSettings(ServletContext ctx) {
         Settings set;
