04c8a0fd6aae79a22c0a30259f036cdaad69bf51
Lucas Holt <luke@foolishgames.com>
2013-05-24 07:33:58 -0400

    add entry and account.

--- src/main/java/com/justjournal/ctl/Entry.java
+++ src/main/java/com/justjournal/ctl/Entry.java
@@ -1,7 +1,87 @@
+/*
+ * Copyright (c) 2013 Lucas Holt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 package com.justjournal.ctl;
+
+import com.justjournal.WebLogin;
+import com.justjournal.db.CommentDao;
+import com.justjournal.db.EntryDAO;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+import java.util.Map;
 
 /**
  * @author Lucas Holt
  */
+@Controller
+@RequestMapping("/json/Entry.json")
 public class Entry {
+    private static final Logger log = Logger.getLogger(Entry.class);
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public
+    @ResponseBody
+    Map<String, String> delete(@RequestBody int entryId, HttpSession session, HttpServletResponse response) throws Exception {
+
+        if (!WebLogin.isAuthenticated(session)) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return java.util.Collections.singletonMap("error", "The login timed out or is invalid.");
+        }
+
+        if (entryId < 1)
+            return Collections.singletonMap("error", "The entry id was invalid.");
+
+        try {
+            boolean result2 = false;
+            boolean result = EntryDAO.delete(entryId, WebLogin.currentLoginId(session));
+
+            if (!result) {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                return java.util.Collections.singletonMap("error", "Could not delete entry.");
+            }
+
+            result2 = CommentDao.deleteByEntry(entryId);
+            if (!result2) {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                return java.util.Collections.singletonMap("error", "Could not delete comments associated with entry.");
+            }
+
+            return java.util.Collections.singletonMap("id", Integer.toString(entryId));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return java.util.Collections.singletonMap("error", "Could not delete the comment.");
+        }
+    }
 }
