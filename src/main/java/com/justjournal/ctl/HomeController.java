0278b66e3be696917ecd0baeb0dbb6a992d26e44
Lucas Holt <luke@foolishgames.com>
2013-05-25 19:41:03 -0400

    setup a model

--- src/main/java/com/justjournal/ctl/HomeController.java
+++ src/main/java/com/justjournal/ctl/HomeController.java
@@ -26,8 +26,13 @@
 
 package com.justjournal.ctl;
 
+import com.justjournal.WebLogin;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * @author Lucas Holt
@@ -35,8 +40,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/")
-    public String home() {
+    @RequestMapping(value = "/", method= RequestMethod.GET)
+    public String home(Model model, HttpSession session) {
+        model.addAttribute("username", WebLogin.currentLoginName(session));
         return "index";
     }
 }
