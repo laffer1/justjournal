0cb4265010cb68ad0ba55a21559acd3d3dc18ea5
Lucas Holt <luke@foolishgames.com>
2013-11-17 17:27:17 -0500

    add account service and start profile code.

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -7,6 +7,22 @@ angular.module('wwwApp', [])
                         templateUrl: 'views/main.html',
                         controller: 'MainCtrl'
                     })
+                    .when('/cancel', {
+                        templateUrl: 'views/cancel.html',
+                        controller: 'CancelCtrl'
+                    })
+                    .when('/members', {
+                        templateUrl: 'views/members.html',
+                        controller: 'MembersCtrl'
+                    })
+                    .when('/moodlist',{
+                        templateUrl: 'views/moodlist.html',
+                        controller: 'MoodlistCtrl'
+                    })
+                    .when('/privacy', {
+                        templateUrl: 'views/privacy.html',
+                        controller: 'PrivacyCtrl'
+                    })
                     .when('/sitemap', {
                         templateUrl: 'views/sitemap.html',
                         controller: 'SitemapCtrl'
