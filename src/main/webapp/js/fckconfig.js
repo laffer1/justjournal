7bdcfdb3d7757016bd0f290a1871e19f248750ff
Lucas Holt <luke@foolishgames.com>
2014-01-04 16:30:48 -0500

    upgrade bootstrap and delete dojo stuff. We’re going angular.

--- src/main/webapp/js/fckconfig.js
+++
@@ -1,5 +0,0 @@
-FCKConfig.ToolbarSets["JJ"] = [
-    ['Bold','Italic', 'Underline','StrikeThrough', '-','Subscript','Superscript', '-', 'JustifyLeft','JustifyCenter','JustifyRight','JustifyFull', '-', 'TextColor','BGColor', 'RemoveFormat'], 
-    ['OrderedList','UnorderedList','-','Outdent','Indent','Blockquote', '-', 'Link','Unlink','-', 'Source', '-','About'],
-	['Style','FontName','FontSize']
-] ;
