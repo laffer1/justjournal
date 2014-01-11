2748968e05dcdc81dbae019336751b1515e6a9b1
Lucas Holt <luke@foolishgames.com>
2014-01-11 12:53:16 -0500

    use util version

--- src/test/java/com/justjournal/db/LocationDaoTest.java
+++ src/test/java/com/justjournal/db/LocationDaoTest.java
@@ -1,5 +1,6 @@
 package com.justjournal.db;
 
+import com.justjournal.Util;
 import com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource;
 import org.apache.cayenne.access.DataContext;
 import org.apache.cayenne.configuration.server.ServerRuntime;
@@ -15,34 +16,8 @@ import static org.junit.Assert.assertEquals;
 
 public class LocationDaoTest {
     @BeforeClass
-    public static void setUpClass() throws Exception {
-        try {
-            // Create initial context
-            System.setProperty(Context.INITIAL_CONTEXT_FACTORY,
-                    "org.apache.naming.java.javaURLContextFactory");
-            System.setProperty(Context.URL_PKG_PREFIXES,
-                    "org.apache.naming");
-            InitialContext ic = new InitialContext();
-
-            ic.createSubcontext("java:");
-            ic.createSubcontext("java:comp");
-            ic.createSubcontext("java:comp/env");
-            ic.createSubcontext("java:comp/env/jdbc");
-
-            // Construct DataSource
-            MysqlConnectionPoolDataSource ds = new MysqlConnectionPoolDataSource();
-            ds.setURL("jdbc:mysql://ds9.midnightbsd.org:3306/jj");
-            ds.setUser("jj");
-            ds.setPassword("");
-
-            ic.bind("java:comp/env/jdbc/jjDB", ds);
-
-            ServerRuntime cayenneRuntime = new ServerRuntime("cayenne-JustJournalDomain.xml");
-            DataContext.bindThreadObjectContext(cayenneRuntime.getContext());
-        } catch (NamingException ex) {
-            System.err.println(ex.getMessage());
-        }
-
+    public static void setup() throws Exception {
+        Util.setupDb();
     }
 
     @Test
