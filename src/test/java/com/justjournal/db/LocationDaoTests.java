79082cce2ea09cb770023895aa3fd06d233ccc02
Lucas Holt <luke@foolishgames.com>
2014-01-13 19:09:10 -0500

    remove useless includes

--- src/test/java/com/justjournal/db/LocationDaoTests.java
+++ src/test/java/com/justjournal/db/LocationDaoTests.java
@@ -1,16 +1,8 @@
 package com.justjournal.db;
 
 import com.justjournal.Util;
-import com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource;
-import org.apache.cayenne.access.DataContext;
-import org.apache.cayenne.configuration.server.ServerRuntime;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 import static org.junit.Assert.assertEquals;
 
