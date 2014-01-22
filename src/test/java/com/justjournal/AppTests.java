3ae50a467b640e0d3408d00e9565a2b4a06cf50f
Lucas Holt <luke@foolishgames.com>
2014-01-21 20:48:02 -0500

    wire up some new unit tests

--- src/test/java/com/justjournal/AppTests.java
+++ src/test/java/com/justjournal/AppTests.java
@@ -27,20 +27,21 @@
 package com.justjournal;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -81,12 +82,60 @@ public class AppTests {
     @Test
     public void apiStatistics() throws Exception {
         mockMvc.perform(get("/api/statistics"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith("application/json;charset=UTF-8"));
     }
 
     @Test
     public void apiTags() throws Exception {
         mockMvc.perform(get("/api/tags"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith("application/json;charset=UTF-8"));
+    }
+
+    @Test
+    public void apiEntryPost() throws Exception {
+
+        mockMvc.perform(post("/api/entry", "{\"id\":\"1\", \"subject\":\"testing\", \"body\":\"test\"}")
+                .content("{\"id\":\"1\", \"subject\":\"testing\", \"body\":\"test\"}")
+                .contentType(MediaType.APPLICATION_JSON)
+
+                .accept(MediaType.parseMediaType("application/json")))
+                .andExpect(status().is(403))
+                .andExpect(content().string("{\"error\":\"The login timed out or is invalid.\"}"))
+                .andExpect(content().contentTypeCompatibleWith("application/json"));
+    }
+
+    @Test
+    public void apiEntry() throws Exception {
+        mockMvc.perform(get("/api/entry/laffer1/33626").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith("application/json;charset=UTF-8"));
+    }
+
+    @Test
+    public void apiEntryWithUser() throws Exception {
+        mockMvc.perform(get("/api/entry/jjsite").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith("application/json;charset=UTF-8"));
+    }
+
+
+    @Test
+    public void apiLoginCheck() throws Exception {
+        mockMvc.perform(get("/api/login").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith("application/json;charset=UTF-8"));
+    }
+
+
+    @Test
+    public void apiLogin() throws Exception {
+        mockMvc.perform(post("/api/login", "{\"username\":\"laffer1\", \"password\":\"blah\"}")
+                .accept(MediaType.parseMediaType("application/json"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"username\":\"laffer1\", \"password\":\"blah\"}"))
+                .andExpect(status().is(403))
+                .andExpect(content().contentTypeCompatibleWith("application/json;charset=UTF-8"));
     }
 }
