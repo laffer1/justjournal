1cdebb27a812b55f0b2ddc1976897d085d85bdc3
Lucas Holt <luke@foolishgames.com>
2014-02-11 21:42:31 -0500

    entry to tags

---
+++ src/main/java/com/justjournal/model/EntryTags.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2014 Lucas Holt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+package com.justjournal.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+
+/**
+ * Entry To Tags relationship
+ *
+ * @author Lucas Holt
+ */
+@Entity
+@Table(name = "entry_tags")
+public class EntryTags {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @JsonProperty("entry")
+    @ManyToOne
+    @JoinColumn(name = "entryid")
+    private Entry entry;
+
+    @JsonProperty("tag")
+    @ManyToOne
+    @JoinColumn(name = "tagid")
+    private Tag tag;
+
+    @JsonCreator
+    public EntryTags() {
+    }
+
+    public Entry getEntry() {
+        return entry;
+    }
+
+    public void setEntry(final Entry entry) {
+        this.entry = entry;
+    }
+
+    public Tag getTag() {
+        return tag;
+    }
+
+    public void setTag(final Tag tag) {
+        this.tag = tag;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+}
