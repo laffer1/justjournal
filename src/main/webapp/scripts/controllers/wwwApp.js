3b89a0248d639b6235fc85cf9218a503b8fc92c1
Lucas Holt <luke@foolishgames.com>
2014-03-21 00:49:40 -0400

    update tracking code.

--- src/main/webapp/scripts/controllers/wwwApp.js
+++ src/main/webapp/scripts/controllers/wwwApp.js
@@ -1,11 +1,14 @@
 angular.module('wwwApp').controller('wwwAppCtrl',
-        ['$scope', '$routeParams', '$location', 'LoginService',
-            function ($scope, $routeParams, $location, LoginService) {
+        ['$scope', '$routeParams', '$location', 'LoginService', '$window',
+            function ($scope, $routeParams, $location, LoginService, $window) {
                 'use strict';
 
                 $scope.Login = LoginService;
 
                 $scope.$on('$destroy', function finalize() {
                     $scope.Login = null;
+                });
+                $scope.$on('$viewContentLoaded', function () {
+                    $window.ga('send', 'pageview');
                 });
             }]);
