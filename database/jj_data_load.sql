1208bbc56675b33d58aee10fcc952e886745c089
Lucas Holt <luke@foolishgames.com>
2014-03-29 11:26:13 -0400

    add some test data.

--- database/jj_data_load.sql
+++ database/jj_data_load.sql
@@ -1085,3 +1085,10 @@ VALUES
 INSERT INTO `user_pref` (`id`, `user_id`, `allow_spider`, `style`, `owner_view_only`, `show_avatar`, `journal_name`, `ping_services`, `modified`)
 VALUES
   (2192, 2908, 'Y', 1, 'N', 'N', 'Testing Journal', 'N', '2014-03-29 15:12:53');
+
+INSERT INTO `justjournal_test`.`entry` (`id`, `uid`, `date`, `modified`, `subject`, `mood`, `music`, `location`, `body`, `security`, `autoformat`, `allow_comments`, `email_comments`, `draft`, `attach_image`, `attach_file`)
+VALUES (NULL, '2908', CURRENT_DATE(), CURRENT_TIMESTAMP, 'test subject', '2', 'the rock band song', '2',
+        'i am a body of a blog post in text format', '2', 'Y', 'Y', 'Y', 'N', '0', '0');
+INSERT INTO `justjournal_test`.`entry` (`id`, `uid`, `date`, `modified`, `subject`, `mood`, `music`, `location`, `body`, `security`, `autoformat`, `allow_comments`, `email_comments`, `draft`, `attach_image`, `attach_file`)
+VALUES (NULL, '2908', CURRENT_DATE(), CURRENT_TIMESTAMP, 'test subject 2', '3', 'the rock band song', '20',
+        'i am a body of a blog post in text format', '1', 'Y', 'Y', 'Y', 'N', '0', '0');
