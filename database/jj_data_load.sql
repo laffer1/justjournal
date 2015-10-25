dfb26f90892ef1332560692781440e4cbe56336a
Lucas Holt <luke@foolishgames.com>
2015-10-25 11:14:00 -0400

    add 19 bootstrap theme

--- database/jj_data_load.sql
+++ database/jj_data_load.sql
@@ -484,13 +484,11 @@ INSERT INTO `style` (`id`, `title`, `desc`, `modified`) VALUES
   (10, 'Modern Red', 'Fire engines eat your heart out.', '2012-06-20 03:42:57'),
   (11, 'Deep Blue Ocean', 'Created by Geomaniak', '2012-06-20 03:42:57'),
   (12, 'Gold and Blue', 'A subtle theme based on Modern.', '2012-06-20 03:42:57'),
-  (13, 'Modern Black', 'A dark style', '2012-06-20 03:42:57'),
-  (14, 'Modern Right Black', 'A reverse layout', '2012-06-20 03:42:57'),
   (15, 'Colors', 'A bright theme', '2012-06-20 03:42:57'),
   (16, 'Ugly Orange', 'Orange', '2012-06-20 03:42:57'),
   (17, 'Cerulean', 'A calm blue sky', '2015-10-25 10:42:57'),
-  (18, 'Cosmo', 'An ode to metro', '2015-10-25 10:42:57'),
-;
+  (18, 'Cosmo', 'An ode to metro', '2015-10-25 10:42:57');
+insert into style VALUES(19, 'Cyborg', 'Jet black and electric blue', '2015-10-25 10:42:57');
 
 --
 -- Dumping data for table `timezones`
