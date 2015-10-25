0bf1fa3adc931ac4c87e69b96b638f449b5462d9
Lucas Holt <luke@foolishgames.com>
2015-10-25 11:06:03 -0400

    add a new theme

--- database/jj_data_load.sql
+++ database/jj_data_load.sql
@@ -487,7 +487,8 @@ INSERT INTO `style` (`id`, `title`, `desc`, `modified`) VALUES
   (13, 'Modern Black', 'A dark style', '2012-06-20 03:42:57'),
   (14, 'Modern Right Black', 'A reverse layout', '2012-06-20 03:42:57'),
   (15, 'Colors', 'A bright theme', '2012-06-20 03:42:57'),
-  (16, 'Ugly Orange', 'Orange', '2012-06-20 03:42:57');
+  (16, 'Ugly Orange', 'Orange', '2012-06-20 03:42:57'),
+  (17, 'Cerulean', 'A calm blue sky', '2015-10-25 10:42:57');
 
 --
 -- Dumping data for table `timezones`
