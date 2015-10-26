ae42cdee3714036f0aac80288e703dbda3eb6542
Lucas Holt <luke@foolishgames.com>
2015-10-26 08:54:44 -0400

    add readable

--- database/jj_data_load.sql
+++ database/jj_data_load.sql
@@ -491,6 +491,7 @@ insert into style VALUES(19, 'Cyborg', 'Jet black and electric blue', '2015-10-2
 insert into style VALUES(20, 'Journal', 'Crisp like a new sheet of paper', '2015-10-25 10:42:57');
 insert into style VALUES(21, 'Lumen', 'Light and shadow', '2015-10-25 10:42:57');
 insert into style VALUES(22, 'Paper', 'Material is the metaphor', '2015-10-25 10:42:57');
+insert into style VALUES(23, 'Readable', 'Optimized for legibility', '2015-10-25 10:42:57');
 
 --
 -- Dumping data for table `timezones`
