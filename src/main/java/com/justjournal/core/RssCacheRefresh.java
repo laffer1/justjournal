7a2da265fa5692cc63b7ef874c7e6cd646ce3768
Lucas Holt <luke@foolishgames.com>
2016-07-12 19:36:59 -0400

    livejournal returns 410s so just say screw it and turn them off

--- src/main/java/com/justjournal/core/RssCacheRefresh.java
+++ src/main/java/com/justjournal/core/RssCacheRefresh.java
@@ -29,27 +29,23 @@ package com.justjournal.core;
 import com.justjournal.model.RssCache;
 import com.justjournal.repository.RssCacheDao;
 import com.justjournal.utility.StringUtil;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import static java.lang.Thread.sleep;
-import static java.lang.Thread.yield;
 
 /**
  * Update the RSS cache
@@ -73,56 +69,78 @@ public class RssCacheRefresh {
 
             final List<RssCache> items = rssCacheDao.findByLastUpdatedBefore(yesterday.getTime());
 
-             for (final RssCache cache : items) {
-                 try {
-                     getRssDocument(cache);
-                     sleep(200);
-                 } catch (final Exception e) {
+            for (final RssCache cache : items) {
+                try {
+                    if (!cache.getActive())
+                        continue;
+
+                    getRssDocument(cache);
+                    sleep(200);
+                } catch (final Exception e) {
                     log.error(e.getMessage());
-                 }
-             }
+                }
+            }
 
         } catch (final Exception e) {
-          log.error(e.getMessage());
+            log.error(e.getMessage());
         }
 
-       log.trace("RssCache: Quit");
+        log.trace("RssCache: Quit");
     }
 
     /**
      * Fetch the RSS feed.
+     *
      * @param rss
      * @throws Exception
      */
     void getRssDocument(final RssCache rss) throws Exception {
-
-        URL u;
-        InputStreamReader ir;
-        StringBuilder sbx = new StringBuilder();
-        BufferedReader buff;
+        final StringBuilder sbx = new StringBuilder();
 
         if (rss != null && rss.getUri() != null && rss.getUri().length() > 10) {
+            final HttpClient client = HttpClientBuilder.create().build();
+            final HttpGet request = new HttpGet(rss.getUri());
+            final HttpResponse response = client.execute(request);
 
-            u = new URL(rss.getUri());
-            ir = new InputStreamReader(u.openStream(), "UTF-8");
-            buff = new BufferedReader(ir);
-            String input;
-            while ((input = buff.readLine()) != null) {
-                sbx.append(StringUtil.replace(input, '\'', "\\\'"));
+            final int code = response.getStatusLine().getStatusCode();
+            if (code == 200) {
+                final HttpEntity entity = response.getEntity();
+                final byte[] buffer = new byte[1024];
+                if (entity != null) {
+                    final InputStream inputStream = entity.getContent();
+                    try {
+                        int bytesRead = 0;
+                        final BufferedInputStream bis = new BufferedInputStream(inputStream);
+                        while ((bytesRead = bis.read(buffer)) != -1) {
+                            final String chunk = new String(buffer, 0, bytesRead);
+                            sbx.append(StringUtil.replace(chunk, '\'', "\\\'"));
+                        }
+                        bis.close();
+                    } catch (final Exception e) {
+                        log.error(e.getMessage());
+                    }
+                }
+
+                rss.setLastUpdated(Calendar.getInstance().getTime());
+                rss.setContent(sbx.toString().trim());
+                // sun can't make their own rss feeds complaint
+                if (rss.getContent().startsWith("<rss"))
+                    rss.setContent("<?xml version=\"1.0\"?>\n" + rss.getContent());
+
+                // TODO: we should remove it if this keeps happening somehow.
+                if (rss.getContent().startsWith("<html") || rss.getContent().startsWith("<!DOCTYPE HTML"))
+                    rss.setContent(""); // it's an html page.. bad
+
+                rssCacheDao.saveAndFlush(rss);
+            } else if (code == 404 || code == 410) {
+                log.warn("URL " + rss.getUri() + " is returning a 404. Removing from list");
+
+                rss.setLastUpdated(Calendar.getInstance().getTime());
+                rss.setActive(false);
+                rssCacheDao.saveAndFlush(rss);
+            } else {
+                log.warn("RssCache status code " + code + " for url " + rss.getUri());
             }
-            buff.close();
-
-            rss.setLastUpdated(Calendar.getInstance().getTime());
-            rss.setContent(sbx.toString().trim());
-            // sun can't make their own rss feeds complaint
-            if (rss.getContent().startsWith("<rss"))
-                rss.setContent("<?xml version=\"1.0\"?>\n" + rss.getContent());
-
-            // TODO: we should remove it if this keeps happening somehow.
-            if (rss.getContent().startsWith("<html") || rss.getContent().startsWith("<!DOCTYPE HTML"))
-                rss.setContent(""); // it's an html page.. bad
-            rssCacheDao.saveAndFlush(rss);
         }
     }
-
 }
