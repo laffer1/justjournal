aa173236181f9030219e54e66c707f690dc7083a
Lucas Holt <luke@foolishgames.com>
2016-09-04 14:39:08 -0400

    add CORS filter v1

---
+++ src/main/java/com/justjournal/filter/SimpleCorsFilter.java
@@ -0,0 +1,40 @@
+package com.justjournal.filter;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Lucas Holt
+ */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SimpleCorsFilter implements Filter {
+
+	/** {@inheritDoc} */
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) res;
+		HttpServletRequest request = (HttpServletRequest) req;
+
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "x-auth-token, x-requested-with, accept, authorization, content-type");
+
+		if (!request.getMethod().equals("OPTIONS")) chain.doFilter(req, res);
+	}
+
+	/** {@inheritDoc} */
+	public void init(FilterConfig filterConfig) {}
+
+	/**
+	 * <p>destroy.</p>
+	 */
+	public void destroy() {}
+
+}
