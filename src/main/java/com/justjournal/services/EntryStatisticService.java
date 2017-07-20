cea8c01741c2786bc9092ca4566d5a02c490dd4c
Lucas Holt <luke@foolishgames.com>
2017-07-20 19:27:45 -0400

    switch to constructor and tweak transactions

--- src/main/java/com/justjournal/services/EntryStatisticService.java
+++ src/main/java/com/justjournal/services/EntryStatisticService.java
@@ -12,37 +12,46 @@ import io.reactivex.schedulers.Schedulers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.concurrent.Callable;
 
 /**
  * Compute yearly entry statistics
+ *
  * @author Lucas Holt
  */
 @Service
 @Slf4j
 public class EntryStatisticService {
 
-    @Autowired
-    private EntryStatisticRepository entryStatisticRepository;
+    private final EntryStatisticRepository entryStatisticRepository;
+
+    private final EntryRepository entryRepository;
 
     @Autowired
-    private EntryRepository entryRepository;
+    public EntryStatisticService(final EntryStatisticRepository entryStatisticRepository, final EntryRepository entryRepository) {
+        this.entryStatisticRepository = entryStatisticRepository;
+        this.entryRepository = entryRepository;
+    }
 
-    @Transactional(value = Transactional.TxType.SUPPORTS)
+
+    @Transactional(readOnly = true, isolation = Isolation.READ_UNCOMMITTED)
     public Maybe<EntryStatistic> getEntryCount(final String username, final int year) {
         log.trace("Fetching entry count for " + username + " and year " + year);
 
         return Maybe.just(entryStatisticRepository.findByUsernameAndYear(username, year));
     }
 
+    @Transactional(readOnly = true, isolation = Isolation.READ_UNCOMMITTED)
     public Observable<EntryStatistic> getEntryCounts(final String username) {
         return Observable.fromIterable(entryStatisticRepository.findByUsernameOrderByYearDesc(username));
     }
 
+    @Transactional(isolation = Isolation.READ_UNCOMMITTED)
     public io.reactivex.Observable<EntryStatistic> compute(final User user) {
         final GregorianCalendar calendarg = new GregorianCalendar();
         final int yearNow = calendarg.get(Calendar.YEAR);
@@ -51,6 +60,7 @@ public class EntryStatisticService {
     }
 
 
+    @Transactional(isolation = Isolation.READ_UNCOMMITTED)
     public io.reactivex.Observable<EntryStatistic> compute(final User user, final int startYear, final int endYear) {
         final GregorianCalendar calendarg = new GregorianCalendar();
 
