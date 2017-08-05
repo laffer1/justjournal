747ba9f350d93c2998834c79cf9a4124fa401618
Lucas Holt <luke@foolishgames.com>
2017-08-04 22:00:28 -0400

    support auto links and tables.

--- src/main/java/com/justjournal/services/MarkdownService.java
+++ src/main/java/com/justjournal/services/MarkdownService.java
@@ -1,11 +1,17 @@
 package com.justjournal.services;
 
 import lombok.extern.slf4j.Slf4j;
+import org.commonmark.Extension;
+import org.commonmark.ext.autolink.AutolinkExtension;
+import org.commonmark.ext.gfm.tables.TablesExtension;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 import org.commonmark.renderer.text.TextContentRenderer;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Create a simple service to generate HTML from markdown code.
@@ -17,19 +23,24 @@ import org.springframework.stereotype.Service;
 public class MarkdownService {
 
     // TODO: figure out extensions we want https://github.com/atlassian/commonmark-java
-    
+
     public String convertToHtml(final String markdown) {
-        final HtmlRenderer renderer = HtmlRenderer.builder().build();
+        final HtmlRenderer renderer = HtmlRenderer.builder().extensions(extensions()).build();
         return renderer.render(parse(markdown));
     }
 
     public String convertToText(final String markdown) {
-        final TextContentRenderer renderer = TextContentRenderer.builder().build();
+        final TextContentRenderer renderer = TextContentRenderer.builder().extensions(extensions()).build();
         return renderer.render(parse(markdown));
     }
 
     public Node parse(final String markdown) {
-        final Parser parser = Parser.builder().build();
+        final Parser parser = Parser.builder().extensions(extensions()).build();
         return parser.parse(markdown);
+    }
+
+    public List<Extension> extensions() {
+        List<Extension> extensions = Arrays.asList(TablesExtension.create(), AutolinkExtension.create());
+        return extensions;
     }
 }
