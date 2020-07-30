fbf75c19cdcda69482b065cba58ce9d2515ed0d1
Lucas Holt <luke@foolishgames.com>
2020-07-29 23:01:01 -0400

    switch to travis builds

--- sonar.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar \
-    -Dsonar.host.url=https://sonarcloud.io \
-    -Dsonar.organization=laffer1-github \
-    -Dsonar.login=916d745066722e1fe6ecc2213a8aada185da67a2
