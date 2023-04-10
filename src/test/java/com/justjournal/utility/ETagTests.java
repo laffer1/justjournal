6ab5a463639d22d9a88f0b36efcf28a4a3f3f933
Lucas Holt <luke@foolishgames.com>
2023-04-10 18:13:20 -0400

    add test

---
+++ src/test/java/com/justjournal/utility/ETagTests.java
@@ -0,0 +1,25 @@
+package com.justjournal.utility;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static org.mockito.Mockito.mock;
+
+class ETagTests {
+
+    @Test
+    void testWriteFromString_null() {
+        var resp = mock(HttpServletResponse.class);
+        var etag = new ETag(resp);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> etag.writeFromString(null));
+    }
+
+    @Test
+    void testWrite_null() {
+        var resp = mock(HttpServletResponse.class);
+        var etag = new ETag(resp);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> etag.write(null));
+    }
+}
