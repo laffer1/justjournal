57666bf2435e9dc8cf7a4b729d4dbf9b0f06119c
Lucas Holt <luke@foolishgames.com>
2023-04-20 16:24:46 -0400

    add bing service.

---
+++ src/main/java/com/justjournal/services/BingService.java
@@ -0,0 +1,43 @@
+package com.justjournal.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.owasp.esapi.ESAPI;
+import org.owasp.esapi.errors.EncodingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+public class BingService {
+
+    @Value("${bing.indexNowKey}")
+    private String indexNowKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public boolean indexNow(String permalink) {
+        try {
+            final String cleanPermanentBlogEntryUrl = ESAPI.encoder().encodeForURL(permalink);
+
+            final URI uri =
+                    new URI(
+                            "https://www.bing.com/indexnow?url="
+                                    + cleanPermanentBlogEntryUrl
+                                    + "&key="
+                                    + indexNowKey);
+
+            ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+
+            return result.getStatusCode().is2xxSuccessful();
+        } catch (final Exception me) {
+            log.error("Failed to perform bing index now", me);
+            return false;
+        }
+    }
+}
